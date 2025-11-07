ae16736c1d50d31b2f42b45353f83bfff4299f80
James Kerrigan <jamesjkerrigan05@gmail.com>
2025-11-07 11:40:16 -0500

    finished

--- src/main/java/com/example/crudapi/largeFelines/largeFelineController.java
+++ src/main/java/com/example/crudapi/largeFelines/largeFelineController.java
@@ -2,10 +2,9 @@ package com.example.crudapi.largeFelines;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,7 +17,6 @@ public class largeFelineController {
 
     @GetMapping({"/felines", "/felines/"})
     public Object getAllFelines(Model model) {
-        System.out.println("Getting all felines");
         model.addAttribute("felines", service.getAllFelines());
         model.addAttribute("title", "Large Felines");
         return "felines-list";
@@ -34,7 +32,7 @@ public class largeFelineController {
     
 
     @GetMapping("/felines/name")
-    public Object getFelinesByName(@RequestParam String name, Model model) {
+    public Object getFelinesByName(@PathVariable String name, Model model) {
         if (name != null) {
             model.addAttribute("felines", service.getFelinesByName(name));
             model.addAttribute("title", "Felines Named " + name);
@@ -46,21 +44,21 @@ public class largeFelineController {
         }
 
     @GetMapping("/felines/habitat/{habitat}")
-    public Object getFelinesByHabitat(@RequestParam String habitat, Model model) {
+    public Object getFelinesByHabitat(@PathVariable String habitat, Model model) {
         model.addAttribute("felines", service.getFelinesByHabitat(habitat));
         model.addAttribute("title", "Felines in Habitat: " + habitat);
         return "felines-list";
     }
 
     @GetMapping("/felines/population/{population}")
-    public Object getFelinesByPopulationGreaterThan(@RequestParam String population, Model model) {
+    public Object getFelinesByPopulationGreaterThan(@PathVariable String population, Model model) {
         model.addAttribute("felines", service.getFelinesByPopulationGreaterThan(Integer.parseInt(population)));
         model.addAttribute("title", "Felines with Population Greater Than: " + population);
         return "felines-list";
     }
 
     @GetMapping("/felines/weight/{weight}")
-    public Object getFelinesByWeightGreaterThan(@RequestParam String weight, Model model) {
+    public Object getFelinesByWeightGreaterThan(@PathVariable String weight, Model model) {
        model.addAttribute("felines", service.getFelinesByWeightGreaterThan(Double.parseDouble(weight)));
        model.addAttribute("title", "Felines with Weight Greater Than: " + weight);
          return "felines-list";
@@ -83,13 +81,13 @@ public class largeFelineController {
     }
 
     @PostMapping("/felines")
-    public Object createFeline(@RequestBody largeFeline feline) {
-        largeFeline createdFeline = (largeFeline) service.createFeline(feline);
-        return "redirect:/felines/" + createdFeline.getId();
+    public String createFeline(@ModelAttribute largeFeline feline) {
+        service.createFeline(feline);
+        return "redirect:/felines/" + feline.getId();
     }
 
     @PostMapping("/felines/update/{id}")
-    public Object updateFeline(@PathVariable("id") Long id, @RequestBody largeFeline feline) {
+    public Object updateFeline(@PathVariable("id") Long id, @ModelAttribute largeFeline feline) {
         service.updateFeline(id, feline);
         return "redirect:/felines/" + id;
     }
